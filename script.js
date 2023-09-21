d4d5010ae98225ba241e46bb20238f09a5e2fa24
Erick Daniel Teixeira Vier <126730119+ErickDaniel7@users.noreply.github.com>
2023-09-21 19:09:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function generateQRCode() {
             window.localStorage.setItem('fileSize', fileSize);
 
             // URL da página de informações do arquivo
-            const infoPageUrl = `qr-share-theta.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(file.name)}`;
+            const infoPageUrl = `https://qr-share-theta.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(file.name)}`;
 
             // Criar um link que inclui a URL da página de informações
             const qrCodeLink = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(infoPageUrl)}`;
@@ -65,7 +65,7 @@ function generateQRCode() {
             copyButton.style.display = 'block';
 
             // Passar os dados do arquivo para a página de informações
-            const infoPageLink = `qr-share-theta.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(file.name)}&size=${fileSize}`;
+            const infoPageLink = `https://qr-share-theta.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(file.name)}&size=${fileSize}`;
             window.localStorage.setItem('infoPageLink', infoPageLink);
         }
         fileReader.readAsDataURL(file);
@@ -78,7 +78,7 @@ function copyDownloadLink() {
     const fileName = document.getElementById("fileName").textContent;
 
     // Gerar um link para a página de informações do arquivo
-    const infoPageLink = `qr-share-theta.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(fileName)}&link=${encodeURIComponent(downloadLink)}`;
+    const infoPageLink = `https://qr-share-theta.vercel.app/informacoes-do-arquivo.html?name=${encodeURIComponent(fileName)}&link=${encodeURIComponent(downloadLink)}`;
 
     // Copiar o link para a área de transferência
     const tempInput = document.createElement("input");
